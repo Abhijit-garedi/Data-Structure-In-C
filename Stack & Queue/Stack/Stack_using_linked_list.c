763883688da99cbf125d5a86d39a1fc17c4e30ab
Abhijit-garedi <paljustwin@gmail.com>
2024-05-23 23:23:21 +0530

    Stack using linked list

---
+++ Stack & Queue/Stack/Stack_using_linked_list.c
@@ -0,0 +1,66 @@
+//Stack using linked list
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void push();
+void pop();
+void display();
+
+struct node {
+    int data;
+    struct node *next;
+} *top = NULL;
+
+int main() {
+    int choice;
+    while (1) {
+        printf("\nStack Operations:\n1.Push 2.Pop 3.Display 4.Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1: push(); break;
+            case 2: pop(); break;
+            case 3: display(); break;
+            case 4: exit(0);
+            default: printf("Invalid choice\n"); break;
+        }
+    }
+    return 0;
+}
+
+void push() {
+    struct node *var;
+    int ele;
+    var = (struct node*) malloc(sizeof(struct node));
+    printf("Enter a value to be pushed onto the stack: ");
+    scanf("%d", &ele);
+    var->data = ele;
+    var->next = top;
+    top = var;
+}
+
+void pop() {
+    struct node *var = top;
+    if (top == NULL) {
+        printf("\nStack is Empty\n");
+    } else {
+        printf("\nPopped element is: %d\n", var->data);
+        top = top->next;
+        free(var);
+    }
+}
+
+void display() {
+    struct node *var = top;
+    if (top == NULL) {
+        printf("\nStack is Empty\n");
+    } else {
+        printf("\nStack elements:\n");
+        while (var != NULL) {
+            printf("%d\n", var->data);
+            var = var->next;
+        }
+    }
+}
