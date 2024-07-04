5513873f9b34498d2075bcae3762e3905591ca55
Abhijit-garedi <paljustwin@gmail.com>
2024-07-04 19:06:51 +0530

    infix to prefix

--- chapter 4/exp_8_infix_to_prefix.c
+++ chapter 4/exp_8_infix_to_prefix.c
@@ -25,3 +25,23 @@ struct Stack* createStack(unsigned capacity) {
     stack->array = (int*) malloc(stack->capacity * sizeof(int));
     return stack;
 }
+
+int isFull(struct Stack* stack) {
+    return stack->top == stack->capacity - 1;
+}
+
+int isEmpty(struct Stack* stack) {
+    return stack->top == -1;
+}
+
+void push(struct Stack* stack, char item) {
+    if (isFull(stack))
+        return;
+    stack->array[++stack->top] = item;
+}
+
+char pop(struct Stack* stack) {
+    if (isEmpty(stack))
+        return -1;
+    return stack->array[stack->top--];
+}
