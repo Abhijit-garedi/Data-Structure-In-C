a397a154f3eb400e51e2a5a016a98f420f696f23
Abhijit-garedi <paljustwin@gmail.com>
2024-07-05 16:20:01 +0530

    binary tree creating node and preforming preoder

---
+++ chapter 5/BT_nodes_&_pre-order_traversals.c
@@ -0,0 +1,39 @@
+//  simple C program for a binary tree, focusing on creating nodes and performing a pre-order traversal:
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+void preOrder(struct Node* root) {
+    if (root != NULL) {
+        printf("%d ", root->data);
+        preOrder(root->left);
+        preOrder(root->right);
+    }
+}
+
+int main() {
+    struct Node* root = createNode(1);
+    root->left = createNode(2);
+    root->right = createNode(3);
+    root->left->left = createNode(4);
+    root->left->right = createNode(5);
+
+    printf("Pre-order traversal: ");
+    preOrder(root);
+    printf("\n");
+
+    return 0;
+}
